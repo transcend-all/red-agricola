ea6c0716bce3d77209511568b5ca55a18904c3f5
transcend-all <transcendentalguy@gmail.com>
2017-04-17 13:53:44 -0400

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "901854678570-n9ek0b3kbfatv4tpdpr23mu023qhagj4.apps.googleusercontent.com", "kG1Nee2u-S4C9cwwD0hwy8DD", skip_jwt: true
+  provider :google_oauth2, "901854678570-r8hbc6sj6slhr1j9tjn5fuutte9laodg.apps.googleusercontent.com", "Brpkbq2IbOo5tQZ5-vXkuPq4", skip_jwt: true
 end
